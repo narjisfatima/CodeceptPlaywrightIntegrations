78b942610236a4369dab329f97c7612b64289163
Narjis Fatima <narjisfatima987@gmail.com>
2025-12-16 23:09:59 +0530

    Updated show flag and commented default headless mode

--- codecept.conf.ts
+++ codecept.conf.ts
@@ -1,7 +1,7 @@
-import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
+import {setCommonPlugins } from '@codeceptjs/configure';
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
+// setHeadlessWhen(process.env.HEADLESS);
 
 // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
 setCommonPlugins();
@@ -13,7 +13,7 @@ export const config: CodeceptJS.MainConfig = {
     Playwright: {
       browser: 'chromium',
       url: 'http://localhost',
-      show: true
+      show: false
     }
   },
   include: {
